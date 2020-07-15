1afe9ff0198efa631fa51a5418244aa6adedb8d0
Deepika Dash <dashdeepika96@gmail.com>
2020-07-16 01:18:04 +0530

    different images like Gray,Blur,Canny(edge),imgdialation(thickness),imgErod(opposite of dialation)

--- colorChangeToGray.py
+++ colorChangeToGray.py
@@ -1,10 +1,18 @@
 import cv2 
+import numpy as np
 file_path = 'image/start_of_new_life.jpg'
 img = cv2.imread(file_path)
+kernel = np.ones((5,5),np.uint8)
 imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 imgBlur = cv2.GaussianBlur(imgGray,(3,3),0)
+imgCanny = cv2.Canny(img,150,200)
+imgDialation = cv2.dilate(imgCanny,kernel,iterations=1)
+imgEroded = cv2.erode(imgDialation,kernel,iterations=1)
 
 cv2.imshow("Gray Image",imgGray)
 cv2.imshow("Blur Image",imgBlur)
+cv2.imshow("Canny Image",imgCanny)
+cv2.imshow("Dialation Image",imgDialation)
+cv2.imshow("Eroded Image",imgEroded)
 
 cv2.waitKey(0)
